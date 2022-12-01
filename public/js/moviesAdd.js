10931269e3c3890411a056e97737ce3c88908e82
Axel Bustos <106710127+axellbustos@users.noreply.github.com>
2022-12-01 16:21:50 -0300

    moviesAdd done

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function () {
     let titulo = document.querySelector('.moviesAddTitulo')
     let formulario = document.querySelector('#formulario');
     let article = document.querySelector('article');
@@ -7,6 +7,46 @@ window.onload = function(){
     article.classList.add('fondoTransparente');
     formulario.classList.add('fondoCRUD');
 
+    titulo.addEventListener('mouseover', function (e) {
+        titulo.style.color = 'blue'
+    })
+    titulo.addEventListener("mouseout", function (e) {
+        titulo.style.color = 'black'
+    })
 
+    const Titulo = document.getElementById("titulo")
+    let estadoSecreto = 0
+
+    Titulo.addEventListener('keypress',function(e){
+
+        switch (estadoSecreto) {
+            case 0:
+                e.key === "s" ? ++estadoSecreto : estadoSecreto = 0;
+                break;
+            case 1:
+                e.key === "e" ? ++estadoSecreto : estadoSecreto = 0;
+                break;
+            case 2:
+                e.key === "c" ? ++estadoSecreto : estadoSecreto = 0;
+                break;
+            case 3:
+                e.key === "r" ? ++estadoSecreto : estadoSecreto = 0;
+                break;
+            case 4:
+                e.key === "e" ? ++estadoSecreto : estadoSecreto = 0;
+                break;
+            case 5:
+                e.key === "t" ? ++estadoSecreto : estadoSecreto = 0;
+                break;
+            case 6:
+                e.key === "o" ? ++estadoSecreto : estadoSecreto = 0;
+                estadoSecreto === 7 && alert("SECRETO MAGICO")
+                estadoSecreto = 0;
+                break;
+            default:
+                estadoSecreto = 0;
+                break;
+        }
+    })
 
 }
